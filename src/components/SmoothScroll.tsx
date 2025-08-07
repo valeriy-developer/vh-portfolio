42fb21b95a7b1163833e7cd7e5c5f90ee766f9fe
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-08-07 16:40:15 +0300

    feat: updated lenis smooth scroll

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -1,15 +1,28 @@
 "use client";
 
-import ReactLenis from "lenis/react";
-import React from "react";
+import gsap from "gsap";
+import ReactLenis, { LenisRef } from "lenis/react";
+import React, { useEffect, useRef } from "react";
 
 const SmoothScroll = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    const update = (time: number) => {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    };
+
+    gsap.ticker.add(update);
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
   return (
-    <ReactLenis root options={{ lerp: 0.06 }}>
+    <ReactLenis root options={{ lerp: 0.06 }} ref={lenisRef}>
       {children}
     </ReactLenis>
   );
