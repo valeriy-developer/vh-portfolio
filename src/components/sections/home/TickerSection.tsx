acf555667a7d65240f0fc14c9e33c33e35d02573
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-08-28 20:12:13 +0300

    feat: added ticker section

---
+++ src/components/sections/home/TickerSection.tsx
@@ -0,0 +1,51 @@
+import Container from "@/components/Container";
+import Sparkle from "@/components/icons/Sparkle";
+import { technologies } from "@/data/technologies";
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const TickerSection = () => {
+  return (
+    <section className="overflow-hidden pt-20 md:pt-37.5">
+      <Container>
+        <div className="relative flex flex-col gap-3">
+          <div className="from-primary pointer-events-none absolute top-0 -right-1 z-10 h-full w-20 bg-gradient-to-l to-transparent md:w-60" />
+          <div className="from-primary pointer-events-none absolute top-0 -left-1 z-10 h-full w-20 bg-gradient-to-r to-transparent md:w-60" />
+          <Marquee autoFill speed={80} className="overflow-hidden!">
+            <div className="mr-5.5 flex items-center gap-5.5">
+              {technologies.map((technology, idx) => (
+                <div
+                  key={idx}
+                  className="text-accent font-big-shoulders flex items-center gap-5.5 text-[5rem] font-black uppercase max-md:leading-[1.2em] md:text-9xl"
+                >
+                  <p>{technology.label}</p>
+                  <Sparkle className="text-secondary/10 h-8 w-8" />
+                </div>
+              ))}
+            </div>
+          </Marquee>
+          <Marquee
+            direction="right"
+            autoFill
+            speed={80}
+            className="overflow-hidden!"
+          >
+            <div className="mr-5.5 flex items-center gap-5.5">
+              {technologies.map((technology, idx) => (
+                <div
+                  key={idx}
+                  className="text-accent font-big-shoulders flex items-center gap-5.5 text-[5rem] font-black uppercase max-md:leading-[1.2em] md:text-9xl"
+                >
+                  <p>{technology.label}</p>
+                  <Sparkle className="text-secondary/10 h-8 w-8" />
+                </div>
+              ))}
+            </div>
+          </Marquee>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default TickerSection;
--- src/config/sections.config.ts
+++ src/config/sections.config.ts
@@ -2,6 +2,7 @@ import AboutSection from '@/components/sections/home/AboutSection';
 import ExperienceSection from '@/components/sections/home/ExperienceSection';
 import HeroSection from '@/components/sections/home/HeroSection';
 import ProjectsSection from '@/components/sections/home/ProjectsSection';
+import TickerSection from '@/components/sections/home/TickerSection';
 import { Section } from '@/types';
 
 export const homepageSections: Section[] = [
@@ -9,4 +10,5 @@ export const homepageSections: Section[] = [
   { id: "about", component: AboutSection },
   { id: "projects", component: ProjectsSection },
   { id: "experience", component: ExperienceSection },
+  { id: "ticker", component: TickerSection },
 ];
