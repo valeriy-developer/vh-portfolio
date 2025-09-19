6973ae157456f1d659375857e7b9ea5a0f7085ff
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-09-19 11:50:39 +0300

    fix: ticker container

--- src/components/sections/home/TickerSection.tsx
+++ src/components/sections/home/TickerSection.tsx
@@ -7,10 +7,10 @@ import Marquee from "react-fast-marquee";
 const TickerSection = () => {
   return (
     <section className="overflow-hidden pt-20 md:pt-37.5">
-      <Container>
+      <Container className="px-0 md:px-6 lg:px-28">
         <div className="relative flex flex-col gap-3">
-          <div className="easing from-primary via-primary/90 pointer-events-none absolute top-0 -right-1 z-10 h-full w-15 bg-gradient-to-l to-transparent md:w-20" />
-          <div className="easing from-primary via-primary/90 pointer-events-none absolute top-0 -left-1 z-10 h-full w-15 bg-gradient-to-r to-transparent md:w-20" />
+          <div className="easing from-primary via-primary/90 pointer-events-none absolute top-0 -right-1 z-10 h-full w-20 bg-gradient-to-l to-transparent" />
+          <div className="easing from-primary via-primary/90 pointer-events-none absolute top-0 -left-1 z-10 h-full w-20 bg-gradient-to-r to-transparent" />
           <Marquee autoFill speed={80} className="overflow-hidden!">
             <div className="mr-5.5 flex items-center gap-5.5">
               {technologies.map((technology, idx) => (
