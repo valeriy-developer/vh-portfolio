89803248460b99e33096f0914375f5b93a9d75be
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-09-29 19:28:26 +0300

    feat: added animation to all hero pages

--- src/components/AppHeroContent.tsx
+++ src/components/AppHeroContent.tsx
@@ -1,7 +1,9 @@
 import { cn } from "@/lib/utils";
-import React from "react";
+import React, { useRef } from "react";
 import Container from "./Container";
 import Sparkle from "./icons/Sparkle";
+import { usePathname } from "next/navigation";
+import { useGSAP, SplitText, gsap } from "@/lib/gsap";
 
 interface Props {
   title: string;
@@ -9,16 +11,54 @@ interface Props {
 }
 
 const AppHeroContent = ({ title, className }: Props) => {
+  const pathname = usePathname();
+  const sectionRef = useRef<HTMLElement>(null);
+
+  useGSAP(
+    () => {
+      const splitTitle = SplitText.create("[data-title]", {
+        type: "chars",
+      });
+
+      const tl = gsap.timeline();
+
+      tl.from(
+        splitTitle.chars,
+        {
+          y: 60,
+          opacity: 0,
+          stagger: 0.08,
+          duration: 0.4,
+          ease: "power2.inOut",
+        },
+        "+=0.3",
+      );
+      tl.from(
+        "[data-sparkle]",
+        {
+          scale: 0.6,
+          opacity: 0,
+          duration: 0.8,
+          ease: "power2.inOut",
+        },
+        "<50%",
+      );
+    },
+    { scope: sectionRef, dependencies: [pathname] },
+  );
+
   return (
-    <section>
+    <section ref={sectionRef}>
       <Container
         className={cn(
           "col-center font-big-shoulders text-accent easing-text pt-28 text-[5rem] leading-[1em] font-black uppercase md:pt-36 md:text-[11.25rem]",
           className,
         )}
       >
-        <h1>{title}</h1>
-        <Sparkle className="mt-6.5 max-md:h-8 max-md:w-8" />
+        <h1 data-title>{title}</h1>
+        <div data-sparkle>
+          <Sparkle className="mt-6.5 max-md:h-8 max-md:w-8" />
+        </div>
       </Container>
     </section>
   );
