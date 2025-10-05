9a130aac274e5ba3dabee5b391d412b7b8cbd28f
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-10-05 16:52:08 +0300

    chore: some changes

--- src/components/CareerCard.tsx
+++ src/components/CareerCard.tsx
@@ -20,14 +20,11 @@ const CareerCard = ({ career, className }: Props) => {
           "hidden md:[li:nth-child(n+3)_&]:block",
         )}
       />
-
       <h3 className="text-secondary text-xl font-semibold">{career.title}</h3>
-
       <div className="text-secondary/50 mt-2 flex flex-col font-medium">
         <span>{career.organization}</span>
         <span>{career.years}</span>
       </div>
-
       <p className="text-secondary mt-2 text-sm md:mt-5 md:text-base lg:text-lg">
         {career.description}
       </p>
