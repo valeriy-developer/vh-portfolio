fb77659a3d0cf2af7391fd812882e8c57ad4917e
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-10-07 23:04:31 +0300

    feat: added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import Providers from "@/providers";
 import ThemeRevealer from "@/components/ThemeRevealer";
 import Loader from "@/components/Loader";
 import { Toaster } from "@/components/ui/sonner";
+import Script from "next/script";
 
 const bigShoulders = Big_Shoulders_Display({
   variable: "--font-big-shoulders",
@@ -33,6 +34,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P8X4FW98');
+            `,
+          }}
+        />
+      </head>
       <body
         className={cn(
           "scrollbar antialiased",
@@ -40,6 +57,14 @@ export default function RootLayout({
           dmSans.variable,
         )}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P8X4FW98"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Providers>
           <Loader />
           <SmoothScroll>
