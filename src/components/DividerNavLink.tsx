b8a60d07e86d579063302b76f7cceda480df8d7a
valeriy-developer <valeriy.gnybidenko@gmail.com>
2025-10-16 00:35:23 +0300

    fix: divider animation

--- src/components/DividerNavLink.tsx
+++ src/components/DividerNavLink.tsx
@@ -15,11 +15,11 @@ const DividerNavLink = ({ label, url }: NavigationLink) => {
         {label}
       </h3>
       <div data-line className="relative flex h-1 w-full">
-        <span className="bg-secondary-10 h-full w-full" />
-        <span className="bg-accent h-full w-10 animate-[line-pulse_3s_ease-in-out_infinite] transition-all duration-200" />
-        <div className="flex-center absolute top-1/2 right-0 h-7 w-7 -translate-y-1/2">
-          <span className="bg-accent transition-[transform, background-color] absolute top-1/2 block h-1 w-full animate-[arrow-right-pulse_3s_ease-in-out_infinite] duration-200" />
-          <span className="bg-accent transition-[transform, background-color] absolute top-1/2 block h-1 w-full animate-[arrow-left-pulse_3s_ease-in-out_infinite] duration-200" />
+        <span className="bg-secondary-10 h-full w-full rounded-xl" />
+        <span className="bg-accent h-full animate-[line-pulse_3s_ease-in-out_infinite] rounded-xl transition-all duration-200" />
+        <div className="flex-center absolute top-1/2 right-0 h-6 w-6 -translate-y-1/2 md:h-7 md:w-7">
+          <span className="bg-accent transition-[transform, background-color] absolute top-1/2 block h-1 w-full animate-[arrow-right-pulse_3s_ease-in-out_infinite] rounded-xl duration-200" />
+          <span className="bg-accent transition-[transform, background-color] absolute top-1/2 block h-1 w-full animate-[arrow-left-pulse_3s_ease-in-out_infinite] rounded-xl duration-200" />
         </div>
       </div>
     </TransitionLink>
